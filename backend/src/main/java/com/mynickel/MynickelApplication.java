2a13c00e6b331200368f4e3bfaa2c384d9ba3deb
19104146 <19104146@usc.edu.ph>
2024-12-08 19:05:56 +0800

    chore: init

---
+++ backend/src/main/java/com/mynickel/MynickelApplication.java
@@ -0,0 +1,13 @@
+package com.mynickel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MynickelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MynickelApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mynickel/MynickelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mynickel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MynickelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
